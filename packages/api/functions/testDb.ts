288b98381f8a1e83ab4911cb1fa361566673e1bf
François Rullière <kapochamo@gmail.com>
2020-09-08 08:46:07 +0800

    remove useless destroy

--- packages/api/functions/testDb.ts
+++ packages/api/functions/testDb.ts
@@ -1,7 +1,3 @@
 import knex from '../layers/database';
 
-export const handler = async () =>
-  knex('concept')
-    .select()
-    .then(console.log)
-    .then(() => knex.destroy());
+export const handler = async () => knex('concept').select().then(console.log);
